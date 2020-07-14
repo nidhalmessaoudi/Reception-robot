6a9732979d87de3744b5fdbc69d3e34ca374f0fe
NidhalMess25 <nidhalmessaoudi46@gmail.com>
2020-07-14 22:46:27 +0100

    adding a new file about final steps in our projects

---
+++ Goodbye.py
@@ -0,0 +1,12 @@
+# This is the goodbye source code!
+# It is a part of the Reception-robot Project!
+
+print("I hope the stay was good!")
+
+give_key = input("Can you give me the key please!")
+if give_key == "of course" or "yes" or "yeah" :
+    print("Thank you, see you next time!")
+else :
+    print("You have to give me the key please!") # Hahahahaha you can change like you would!    
+
+
